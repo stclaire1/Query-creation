c639bc10e6538a76ac855dd0a5dfdd6d678ba5e1
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-08-29 17:03:36 -0300

    criando exceção

---
+++ src/main/java/iftm/edu/br/query_creation/service/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package iftm.edu.br.query_creation.service.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
