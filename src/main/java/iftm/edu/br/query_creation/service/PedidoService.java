16f5274d866218028a6ef93a6daf68b7e818d300
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-08-29 21:15:26 -0300

    modificando service Pedido

--- src/main/java/iftm/edu/br/query_creation/service/PedidoService.java
+++ src/main/java/iftm/edu/br/query_creation/service/PedidoService.java
@@ -2,41 +2,65 @@ package iftm.edu.br.query_creation.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import iftm.edu.br.query_creation.domain.Pedido;
+import iftm.edu.br.query_creation.repository.PedidoRepository;
 
 @Service
 public class PedidoService {
 
-    public List<Pedido> findByDesconto(Long desconto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findByDesconto'");
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public List<Pedido> getItensSemDesconto() {
+        List<Pedido> pedidos = pedidoRepository.findByDesconto(0);
+        if (pedidos.isEmpty()) {
+            throw new RuntimeException("Nenhum item encontrado");
+        }
+        return pedidos;
     }
 
-    public List<Pedido> findByDescontoGreaterThan(Long desconto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findByDescontoGreaterThan'");
+    public List<Pedido> getItensComDesconto() {
+        List<Pedido> pedidos = pedidoRepository.findByDescontoGreaterThan(0);
+        if (pedidos.isEmpty()) {
+            throw new RuntimeException("Nenhum item encontrado");
+        }
+        return pedidos;
     }
 
-    public List<Pedido> findAllByOrderByValorUnitDesc() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAllByOrderByValorUnitDesc'");
+    public List<Pedido> getItensOrdenadosPorValor() {
+        List<Pedido> pedidos = pedidoRepository.findAllByOrderByValorUnitDesc();
+        if (pedidos.isEmpty()) {
+            throw new RuntimeException("Nenhum item encontrado");
+        }
+        return pedidos;
     }
 
-    public Long findTopByOrderByQuantidadeDesc() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findTopByOrderByQuantidadeDesc'");
+    public Long getProdutoMaisVendido() {
+        Long codProd = pedidoRepository.findTopByOrderByQuantidadeDesc();
+        if (codProd == null) {
+            throw new RuntimeException("Nenhum item encontrado");
+        }
+        return codProd;
     }
 
     public List<Long> findNfWithMoreThan10Units() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findNfWithMoreThan10Units'");
+        List<Long> nfId = pedidoRepository.findNfWithMoreThan10Units();
+        if (nfId.isEmpty()) {
+            throw new RuntimeException("Nenhuma nota fiscal encontrada com mais de 10 unidades vendidas de pelo menos um item");
+        }
+        return nfId;
     }
 
-    public List<Double> findTotalValueOfNfGreaterThan500() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findTotalValueOfNfGreaterThan500'");
+    public List<Object[]> findTotalValueOfNfGreaterThan500() {
+        List<Object[]> nfTotal = pedidoRepository.findTotalValueOfNfGreaterThan500();
+        if (nfTotal.isEmpty()) {
+            throw new RuntimeException("Nenhum nota fiscal encontrada com o valor total maior que 500.0");
+        }
+        return nfTotal;
     }
     
+
 }
